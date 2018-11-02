51bc53749599f1c0a0904ca426de2602bff870a6
Casey Randall <42552122+gospel02@users.noreply.github.com>
2018-11-02 07:47:03 -0600

    Added Courses and Tees

--- myscript.js
+++ myscript.js
@@ -18,26 +18,27 @@ function addplayers(){
         $(".content").append( "<div class='column'>" + 
         "<i class='trashcan far fa-trash-alt'></i>" +
         "<input class='players' contenteditable='true' placeholder='Enter Player " + i + "'></input>" +
-        "<input class='scoreinput' id='1' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='2' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='3' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='4' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='5' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='6' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='7' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='8' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='9' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='1' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='2' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='3' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='4' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='5' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='6' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='7' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='8' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputf' id='9' contenteditable='true' type='number' placeholder='-'></input>" +
         "<input class='scoreinput' id='fronttotal'></input>" +
-        "<input class='scoreinput' id='10' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='11' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='12' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='13' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='14' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='15' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='16' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='17' contenteditable='true' type='number' placeholder='-'></input>" +
-        "<input class='scoreinput' id='18' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='10' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='11' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='12' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='13' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='14' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='15' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='16' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='17' contenteditable='true' type='number' placeholder='-'></input>" +
+        "<input class='scoreinputb' id='18' contenteditable='true' type='number' placeholder='-'></input>" +
         "<input class='scoreinput' id='backtotal'></input>" +
+        "<input class='scoreinputt' id='total'></input>" +
         "</div");
         }
     }    
@@ -53,7 +54,73 @@ function addplayers(){
         });
     });
 
+    $(document).on("change", ".scoreinputf", function() {
+        var sum = 0;
+        $(".scoreinputf").each(function(){
+            sum += +$(this).val();
+        });
+        $("#fronttotal").val(sum);
+    });
+
+
+    
+    $(document).on("change", ".scoreinputb", function() {
+        var sum = 0;
+        $(".scoreinputb").each(function(){
+            sum += +$(this).val();
+        });
+        $("#backtotal").val(sum);
+    });
+
+    $(document).on("change", ".scoreinput", function() {
+        var sum = 0;
+        $(".scoreinput").each(function(){
+            sum += +$(this).val();
+        });
+        $("#total").val(sum);
+    });
+
     
 
 }
+
+(function(){
+    loadDoc();
+})();
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          coursecollection = JSON.parse(this.responseText);
+        console.log(coursecollection);
+
+        for(let i = 0; i < coursecollection.courses.length; i++){
+        $("#courseselect").append("<option value='" + coursecollection.courses[i].id +"'>" + coursecollection.courses[i].name + "</option>");
+        }
+      }
+    };
+    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
+    xhttp.send();
+  }
+
+function loadCourses(courseid) {
+    //set to empty
+    console.log(courseid)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        mycourse = JSON.parse(this.responseText);
+        console.log(mycourse);
+
+        let teearray = mycourse.data.holes[0].teeBoxes;
+        for(let i = 0; i < teearray.length; i++){
+            $("#teeselect").append("<option value='" + i +"'>" + teearray[i].teeType + "</option>");
+        }
+     }
+    };
+    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses/" + courseid, true,);
+    xhttp.send();
+
+}   
 
